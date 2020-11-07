b1e7a0eb720be291e5f1e23e09283ef2f570f50c
Oleksandr Lysakov <36567285+pologora@users.noreply.github.com>
2020-11-07 17:04:51 +0100

    js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const paragraf = document.createElement('p');
 const div = document.querySelector('.paragraf');
-paragraf.textContent = "2";
 const button = document.querySelector('button');
 const addSomething = () => {
     div.appendChild(paragraf);
