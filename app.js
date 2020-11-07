f2337d0517a0123227a8abe3aa0c569b7ba7d630
Oleksandr Lysakov <36567285+pologora@users.noreply.github.com>
2020-11-07 16:52:40 +0100

    add js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const paragraf = document.createElement('p');
 const div = document.querySelector('.paragraf');
 paragraf.textContent = "2";
 const button = document.querySelector('button');
-const addSomething = () => { div.appendChild(paragraf); }
+const addSomething = () => {
+    div.appendChild(paragraf);
+    paragraf.textContent += "2";
+}
 
 
 button.addEventListener("click", addSomething);
